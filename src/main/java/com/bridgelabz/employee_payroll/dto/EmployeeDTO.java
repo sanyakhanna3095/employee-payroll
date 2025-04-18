49ae217f7409e7ffa5711c7458931c739c0d28f9
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-18 14:49:08 +0530

    [Sanya] Add: logging

--- src/main/java/com/bridgelabz/employee_payroll/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employee_payroll/dto/EmployeeDTO.java
@@ -1,5 +1,11 @@
 package com.bridgelabz.employee_payroll.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class EmployeeDTO {
     private String name;
     private int salary;
@@ -8,23 +14,5 @@ public class EmployeeDTO {
     public EmployeeDTO(String name, int salary){
         this.name=name;
         this.salary=salary;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name= name;
-    }
-
-    public int getSalary(){
-        return salary;
-    }
-    public void setSalary(int salary){
-        this.salary= salary;
-    }
-    public String toString(){
-        return "EmployeeDTO{name='" + name + "', salary=" + salary + "}";
     }
 }
