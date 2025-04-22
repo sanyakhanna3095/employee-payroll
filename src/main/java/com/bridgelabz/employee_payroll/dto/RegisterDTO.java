c1af53db1ed18c4c8d7d9eac35c6064ef46d5865
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-22 13:40:31 +0530

    [Sanya] Add: Jwt config

---
+++ src/main/java/com/bridgelabz/employee_payroll/dto/RegisterDTO.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.employee_payroll.dto;
+
+public class RegisterDTO {
+}
--- src/main/java/com/bridgelabz/employee_payroll/services/IJwtUserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.employee_payroll.services;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface IJwtUserDetailsService {
-    UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
-}
